283b5f33d1c902770d699e4170ab8793fa53459d
kyoko0219 <150576725+kyoko0219@users.noreply.github.com>
2024-07-08 21:25:30 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>７章課題</title>
+</head>
+<body>
+    <p>
+        <?php
+        // 連想配列に値を代入する
+        $product = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        // 連想配列を出力する
+        print_r($product);
+        ?>
+    </p>
+</body>
+</html>
