7cb7f807cd0090c52b0c688e5a416b810c67cbf1
kyoko0219 <150576725+kyoko0219@users.noreply.github.com>
2024-08-25 00:37:31 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>5章課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
+
+        echo $score1 ;
+        ?>
+    </p>
+</body>
+</html>
